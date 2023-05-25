e6953dab200c3f5aa178e642a7eefe7a611b5d52
roman9234 <117022365+roman9234@users.noreply.github.com>
2023-05-25 09:22:36 +0300

    Update config.py
    скрыты пароли

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 host_c = "127.0.0.1"
 user_c = "postgres"
-password_c = "164014"
+password_c = "******"
 # password_c = "rootroot"
 db_name_c = "IT_7_lab"
 port_c = 5432
-token = "6090458265:AAEpXEAsnMeQGOyyQ8wS8M6UerUD2izIrqc"
+token = "**********************"
